eda9bbff3ddfd8efddee381c6ce5f06155dec503
kurereishu <150410281+kurereishu@users.noreply.github.com>
2025-05-02 05:19:49 +0000

    Deploying to gh-pages from @ kurereishu/kurereishu.github.io@5422a16b80e10ccc33dff09461c5ab3ce306c196 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,7 +449,7 @@ ninja.data = [{
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:🗿',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
